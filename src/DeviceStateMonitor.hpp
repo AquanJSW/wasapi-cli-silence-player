46b7abeed7db5b8b87f113533e94ab459af76552
Shin Rag <zhdlcc@gmail.com>
2025-06-24 03:02:34 -0700

    finish and cleanup

--- src/DeviceStateMonitor.hpp
+++
@@ -1,6 +0,0 @@
-#ifndef DEVICE_STATE_MONITOR_HPP
-#define DEVICE_STATE_MONITOR_HPP
-#include <mmdeviceapi.h>
-#include <winnt.h>
-HRESULT DeviceStateMonitor(IMMDevice *);
-#endif // DEVICE_STATE_MONITOR_HPP
--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,3 @@
-#include "AudioFile.h"
 #include "stdio.h"
 #include <AudioSessionTypes.h>
 #include <audioclient.h>
@@ -14,10 +13,6 @@
 #include <winscard.h>
 #include <wtypes.h>
 
-#ifdef ENABLE_DEVICE_STATE_MONITOR
-#include "DeviceStateMonitor.hpp"
-#endif // ENABLE_DEVICE_STATE_MONITOR
-
 #define EXIT_ON_ERROR(hr)                                                      \
   if (FAILED(hr)) {                                                            \
     printf("Error: 0x%lx\n", hr);                                              \
@@ -30,54 +25,16 @@
     ptr = NULL;                                                                \
   }
 
-#define RETURN_ON_ERROR(hr)                                                    \
-  if (FAILED(hr))                                                              \
-    return hr;
-
-#define RETURN_ON_ERROR_WITH_MSG(hr, msg)                                      \
+#define EXIT_ON_ERROR_WITH_MSG(hr, msg)                                        \
   if (FAILED(hr)) {                                                            \
     printf("%s\n", msg);                                                       \
-    return hr;                                                                 \
+    goto cleanup;                                                              \
   }
 
 const CLSID CLSID_MMDeviceEnumerator = __uuidof(MMDeviceEnumerator);
 const IID IID_IMMDeviceEnumerator = __uuidof(IMMDeviceEnumerator);
 const IID IID_IAudioClient = __uuidof(IAudioClient);
 const IID IID_IAudioRenderClient = __uuidof(IAudioRenderClient);
-
-HRESULT Playing(IAudioClient *pAudioClient, AudioFile<float> &audioFile) {
-  HRESULT hr = 0;
-  IAudioRenderClient *pRenderClient = NULL;
-  const int numChannels = audioFile.getNumChannels();
-  const int numFrames = audioFile.getNumSamplesPerChannel();
-  int frameIndex = 0;
-  UINT32 padding = 0;
-	UINT32 numFramesAvailable = 0;
-  UINT32 bufferFrameCount = 0;
-
-  printf("Getting buffer size...\n");
-  hr = pAudioClient->GetBufferSize(&bufferFrameCount);
-  RETURN_ON_ERROR(hr);
-
-  printf("Getting render client...\n");
-  hr =
-      pAudioClient->GetService(IID_IAudioRenderClient, (void **)&pRenderClient);
-  RETURN_ON_ERROR(hr);
-
-  while (frameIndex < numFrames) {
-    padding = 0;
-    hr = pAudioClient->GetCurrentPadding(&padding);
-		RETURN_ON_ERROR_WITH_MSG(hr, "Failed to get current padding");
-
-		if (numFramesAvailable == 0) {
-			Sleep(5); // Wait for some frames to be available
-			continue;
-		}
-
-  }
-
-  return hr;
-}
 
 int main(int argc, char *argv[]) {
   HRESULT hr = 0;
@@ -87,8 +44,11 @@ int main(int argc, char *argv[]) {
   IAudioClient *pAudioClient = NULL;
   WAVEFORMATEX *pFormat = NULL;
   REFERENCE_TIME hnsDefaultDevicePeriod = 0;
-  char *filePath = NULL;
-  AudioFile<float> audioFile;
+  IAudioRenderClient *pRenderClient = NULL;
+  UINT32 numBufferFrames = 0;
+  UINT32 numPaddingFrames = 0;
+  UINT32 numAvailableFrames = 0;
+  BYTE *pData = NULL;
 
   printf("CoInitialize...\n");
   hr = CoInitialize(NULL);
@@ -103,17 +63,6 @@ int main(int argc, char *argv[]) {
   hr = pEnumerator->GetDefaultAudioEndpoint(eRender, eConsole, &pDevice);
   EXIT_ON_ERROR(hr);
 
-#ifdef ENABLE_DEVICE_STATE_MONITOR
-  printf("Activating device state monitor...\n");
-  hr = DeviceStateMonitor(pDevice);
-  EXIT_ON_ERROR(hr);
-#endif // ENABLE_DEVICE_STATE_MONITOR
-
-  if (argc < 2) {
-    printf("Usage: %s <audio file path>\n", argv[0]);
-    goto cleanup;
-  }
-
   printf("Creating IAudioClient...\n");
   hr = pDevice->Activate(IID_IAudioClient, CLSCTX_ALL, NULL,
                          (void **)&pAudioClient);
@@ -122,11 +71,6 @@ int main(int argc, char *argv[]) {
   printf("GetMixFormat...\n");
   hr = pAudioClient->GetMixFormat(&pFormat);
   EXIT_ON_ERROR(hr);
-
-  printf("|======================================|\n");
-  printf("Num Channels: %d\n", pFormat->nChannels);
-  printf("Sample Rate: %ld\n", pFormat->nSamplesPerSec);
-  printf("|======================================|\n");
 
   printf("GetDevicePeriod...\n");
   hr = pAudioClient->GetDevicePeriod(&hnsDefaultDevicePeriod, NULL);
@@ -137,32 +81,50 @@ int main(int argc, char *argv[]) {
                                 hnsDefaultDevicePeriod, 0, pFormat, NULL);
   EXIT_ON_ERROR(hr);
 
-  filePath = argv[1];
-  if (!audioFile.load(filePath)) {
-    printf("Failed to load audio file: %s\n", filePath);
-    goto cleanup;
-  }
-  audioFile.printSummary();
+  printf("Getting buffer size...\n");
+  hr = pAudioClient->GetBufferSize(&numBufferFrames);
+  EXIT_ON_ERROR(hr);
 
-  if (pFormat->nSamplesPerSec != audioFile.getSampleRate())
-    audioFile.setSampleRate(pFormat->nSamplesPerSec);
+  printf("Getting render client...\n");
+  hr =
+      pAudioClient->GetService(IID_IAudioRenderClient, (void **)&pRenderClient);
+  EXIT_ON_ERROR(hr);
 
   printf("Starting audio client...\n");
   hr = pAudioClient->Start();
   EXIT_ON_ERROR(hr);
 
-  printf("Playing...\n");
-  hr = Playing(pAudioClient, audioFile);
-  EXIT_ON_ERROR(hr);
+  while (true) {
+    numPaddingFrames = 0;
+
+    hr = pAudioClient->GetCurrentPadding(&numPaddingFrames);
+    EXIT_ON_ERROR_WITH_MSG(hr, "Failed to get current padding");
+
+    numAvailableFrames = numBufferFrames - numPaddingFrames;
+
+    if (numAvailableFrames == 0) {
+      Sleep(5);
+      continue;
+    }
+
+    hr = pRenderClient->GetBuffer(numAvailableFrames, &pData);
+    EXIT_ON_ERROR_WITH_MSG(hr, "Failed to get buffer");
+
+    for (int c = 0; c < pFormat->nChannels; ++c) {
+      // Fill the buffer with silence or audio data
+      memset(pData + c * sizeof(float), 0, numAvailableFrames * sizeof(float));
+    }
+
+    hr = pRenderClient->ReleaseBuffer(numAvailableFrames, 0);
+    EXIT_ON_ERROR_WITH_MSG(hr, "Failed to release buffer");
+  }
 
 cleanup:
-  pAudioClient->Stop();
-  if (pFormat)
-    CoTaskMemFree(pFormat);
-  SAVE_RELEASE(pAudioClient);
-  SAVE_RELEASE(pDevice);
+  CoTaskMemFree(pFormat);
   SAVE_RELEASE(pEnumerator);
-
+  SAVE_RELEASE(pDevice);
+  SAVE_RELEASE(pAudioClient);
+  SAVE_RELEASE(pRenderClient);
   CoUninitialize();
 
   return hr;
